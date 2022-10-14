d9fa7313872deb07b56d50d2651d5f461feecf95
Diego Ferraz <diego_ferraz@mckinsey.com>
2022-10-14 10:31:15 -0300

    refactor: extends the components to allow use of props and adds main page

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,25 +1,29 @@
-import React, { ReactNode } from "react"
+import { ReactNode } from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { clsx } from 'clsx'
 
 export interface TextProps {
-  children: ReactNode
   size?: 'sm' | 'md' | 'lg'
+  children: ReactNode
   asChild?: boolean
+  className?: string
 }
 
-export const Text = ({ children, size = 'md', asChild }: TextProps) => {
+export function Text({ size = 'md', children, asChild, className }: TextProps) {
   const Comp = asChild ? Slot : 'span'
 
   return (
-    <Comp className={clsx(
-      'text-gray-100 font-sans',
-      {
-        'text-xs': size === 'sm',
-        'text-sm': size === 'md',
-        'text-md': size === 'lg'
-      }
-    )}>
+    <Comp 
+      className={clsx(
+        'text-gray-100 font-sans', 
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg',
+        },
+        className,
+      )}
+    >
       {children}
     </Comp>
   )
